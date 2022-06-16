e8cf91b8e30581975f656d8ed671986c3913cf45
Leonardo Nunes <leonunesbs@gmail.com>
2022-06-16 00:10:16 -0300

    fix

--- bank/schema/mutation.py
+++ bank/schema/mutation.py
@@ -19,7 +19,7 @@ class CreatePayment(graphene.Mutation):
     payment = graphene.Field('bank.schema.nodes.PaymentNode')
     payment_created = graphene.Boolean()
 
-    def mutate(self, info, method_id, user_username=None, **kwargs):
+    def mutate(self, info, method_id, atttachment_title=None, attachment=None, user_username=None, **kwargs):
         user = info.context.user
         if not user.is_authenticated:
             raise GraphQLError(_('Unauthenticated.'))
@@ -34,13 +34,9 @@ class CreatePayment(graphene.Mutation):
             amount=kwargs.get('amount'),
             description=kwargs.get('description'),
         )
-
-        attachment_title = kwargs.get('attachment_title')
-        attachment = kwargs.get('attachment')
-
-        if created and attachment and attachment_title:
+        if created and attachment and atttachment_title:
             payment.attachments.create(
-                title=attachment_title,
+                title=atttachment_title,
                 file=attachment,
             )
 
