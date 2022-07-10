6dd2532f7ed253330290fcf3cb1bee49069fae58
Leonardo Nunes <leonunesbs@gmail.com>
2022-07-10 17:19:40 -0300

    wpp url

--- members/admin.py
+++ members/admin.py
@@ -1,13 +1,17 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from members.models import Attachment, Member
 
 
 @admin.register(Member)
 class MemberAdmin(admin.ModelAdmin):
-    list_display = ('name', 'nickname', 'email', 'phone',
+    list_display = ('name', 'nickname', 'email', 'phone', 'whatsapp_url',
                     'rg', 'cpf', 'has_active_membership')
     search_fields = ('email', 'name', 'registration',)
+
+    def whatsapp_url(self, obj):
+        return format_html("<a href='{url}' target='_blank'>{url}</a>", url=f'https://wa.me/55{obj.phone}')
 
 
 @admin.register(Attachment)
